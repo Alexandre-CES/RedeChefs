09e3d7542524485e238ea44b668202d11f7738fa
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-02-29 22:58:31 -0300

    v0.8

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
+//Imports
+    const express = require('express')
+    const app = express()
+    const hbs = require('express-handlebars')
+    const mongoose = require('mongoose') 
+
+//Configs
+    //Handlebars
+        app.engine('hbs', hbs.engine({
+            defaultLayout:'main'
+        }))
+        app.set('view engine', 'hbs')
+    
+    //Mongoose
+        mongoose.Promise = global.Promise
+        mongoose.connect('mongo://127.0.0.1:27017/RedeChefs')
 
 
-
-
-const PORT = 8081
-app.listen(PORT, ()=>{
-    console.log(`Server running at http://Localhost:${PORT}`)
-})
+//App starting
+    const PORT = 8081
+    app.listen(PORT, ()=>{
+        console.log(`Server running at http://Localhost:${PORT}`)
+    })
